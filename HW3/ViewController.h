c11725b666d9676e60cc269032e6fd297fb63f0c
czhang <czhang@iMac.local>
2015-02-03 22:59:03 -0800

    now tied to table view and can add items

--- HW3/ViewController.h
+++ HW3/ViewController.h
@@ -7,15 +7,19 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "TodoList.h"
 
-@interface ViewController : NSViewController <NSTextFieldDelegate>
+@interface ViewController : NSViewController <NSTextFieldDelegate, NSTableViewDataSource, NSTableViewDelegate>
 
 
 @property (weak) IBOutlet NSTextField *textFieldEntry;
 @property (weak) IBOutlet NSButton *buttonAdd;
 @property (weak) IBOutlet NSButton *buttonRemove;
-@property (weak) IBOutlet NSButton *checkDuplication;
-@property (weak) IBOutlet NSTableView *tableTodoList;
+@property (weak) IBOutlet NSButton *allowDupButton;
+
+@property (weak) IBOutlet NSTableView *tabView;
+
+@property TodoList* todoList;
 
 @end
 
--- HW3/ViewController.m
+++ HW3/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "TodoItem.h"
 
 @implementation ViewController
 
@@ -16,6 +17,10 @@
     // Do any additional setup after loading the view.
     
     self.textFieldEntry.delegate = self;
+    self.tabView.delegate = self;
+    self.tabView.dataSource = self;
+    
+    self.todoList = [TodoList groceryList];
 }
 
 - (void)setRepresentedObject:(id)representedObject {
@@ -26,19 +31,56 @@
 
 - (IBAction)onClickButtonAdd:(id)sender {
     NSLog(@"click add");
+    
+    NSString* text = [self.textFieldEntry stringValue];
+    TodoItem* item = [TodoItem todoItemWithTitle:text];
+
+    [self.todoList addItem:item];
+    
+    [self.tabView reloadData];
 }
 
 - (IBAction)onClickRemove:(id)sender {
     NSLog(@"click remove");
+    
 }
 
-- (IBAction)onClickCheckDuplication:(id)sender {
-    NSLog(@"click check dup");
+- (IBAction)allowDupCheck:(id)sender {
+    NSButton* button = (NSButton*)sender;
+    
+    if (button.state == NSOnState)
+    {
+        NSLog(@"ON");
+        self.todoList.allowDuplicates = YES;
+    }
+    else
+    {
+        NSLog(@"OFF");
+        self.todoList.allowDuplicates = NO;
+    }
 }
 
 -(void)controlTextDidChange:(NSNotification *)obj
 {
-    NSLog(@"text did change");
+    NSString* text = [self.textFieldEntry stringValue];
+    NSLog(@"text did change: %@", text);
+}
+
+-(NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
+{
+    NSTableCellView* cell =[tableView makeViewWithIdentifier:@"Cell" owner:nil];
+    
+    id obj = [[self.todoList allItems] objectAtIndex:row];
+    TodoItem* item = (TodoItem*)obj;
+    cell.textField.stringValue = [NSString stringWithFormat:@"%@", item.title ];
+    
+    return cell;
+}
+
+-(NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
+{
+    return self.todoList.itemCount;
     
 }
+
 @end
